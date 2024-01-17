475691f2302a7090a3290eb02a35876f04e935b8
Natalia Kobeleva <cobeleva.natasha@gmail.com>
2024-01-17 18:22:37 +0400

    Tasks 1 and 2 solved

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,36 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let d = Math.pow(b, 2) - 4 * a * c;
+
+	if (d < 0) {
+		return arr;
+	} else if (d === 0) {
+		let root = -b / (2 * a);
+		arr.push(root);
+		return arr;
+	} else {
+		let firstRoot = (-b + Math.sqrt(d)) / (2 * a);
+		let secondRoot = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(firstRoot, secondRoot);
+		return arr
+	}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+		percent = Number(percent);
+		contribution = Number(contribution);
+		amount = Number(amount);
+		countMonths = Number(countMonths);
+	} else {
+		return false;
+	}
+
+	let monthlyPercent = percent / 12
+	let creditBody = amount - contribution;
+	let monthlyPayment = creditBody * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1)));
+	let totalMortgage = (monthlyPayment * countMonths).toFixed(2);
+	return Number(totalMortgage);
 }
